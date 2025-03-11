25d29506d35fbd33171cea37f22f18cfd4c8461a
Felix Mayer <FelixM@yer.tools>
2025-03-11 01:49:52 +0100

    Add DaisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import type { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 
 const App: Component = () => {
+  const [count, setCount] = createSignal(0);
+
   return (
-    <p class="py-20 text-center text-4xl text-green-700">Hello tailwind!</p>
+    <div>
+      <p class="py-20 text-center text-4xl text-green-700">Hello tailwind!</p>
+      <button
+        class="btn"
+        onClick={() => setCount(count() + 1)}
+      >
+        Click Me!
+      </button>
+      <span class="countdown font-mono text-6xl">
+        <span
+          {...{ style: `--value:${count()};` }}
+          aria-live="polite"
+          aria-label={count() + ""}
+        >
+          {count()}
+        </span>
+      </span>
+    </div>
   );
 };
 
