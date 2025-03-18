261963bd9b5048fe62b2660672a07150192791ef
Felix Mayer <FelixM@yer.tools>
2025-03-18 20:39:46 +0100

    Improved eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -133,6 +133,18 @@ export default [
   {
     "rules": {
       "prettier/prettier": ["warn"],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "args": "all",
+          "argsIgnorePattern": "^_",
+          "caughtErrors": "all",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "ignoreRestSiblings": true,
+        },
+      ],
     },
   },
 ];
